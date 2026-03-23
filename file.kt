745d6a17ca5cf94e8e98f00183c071e2de0b13f6
tacetfive <tacetfive@gmail.com>
2026-03-23 11:59:02 +0300

    Initial commit

---
+++ file.kt
@@ -0,0 +1,15 @@
+class elementUnique {
+	val title: String
+	val titleShort: String
+	val turn: Int
+	var flip: Int
+	var points: Double
+
+}
+
+class elementSequence : elementUnique {
+	var previousElement: ?
+	var nextElement: ?
+
+
+
