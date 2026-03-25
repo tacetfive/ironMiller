23eec664834a9c1d8cf1cb3013140902658eeb32
tacetfive <tacetfive@gmail.com>
2026-03-25 19:59:29 +0300

    add code

--- file.kt
+++ file.kt
@@ -1,15 +1,48 @@
-class elementUnique {
-	val title: String
-	val titleShort: String
-	val turn: Int
-	var flip: Int
-	var points: Double
+class ElementUnique {
+    val title: String
+    val titleShort: String
+    val turn: Int
+    var flip: Int
+    var points: Double
 
 }
 
-class elementSequence : elementUnique {
-	var previousElement: ?
-	var nextElement: ?
+class ElementSequence : ElementUnique {
+    var previousElement: ?
+    var nextElement: ?
+    fun elementSequence() {
+	var elemSeq: List<ElementUnique>
+    }
+    fun addElement(sequenceID: List<ElementUnique>) { // add element into existing sequence
+	sequenceID.add()
+    }
+}
+
+class ElementProgram : ElementSequence {
+    var title: String
+    var sum: Double
+    fun elementProgram() {
+	addElement()
+    }
+    fun addElement() { // add an unique element
+	
+    }
+
+}
+
+class Profile {
+    var profileName: String
+}
 
 
+fun main {
+    create/choose/open_recent profile
+    user actions:
+	if user press "add element" first time
+	    call createProgram
+	if user press "add element"
+	    call addElement
+	
+    Program checks:
+	are there duplicates in elementProgram;
 
